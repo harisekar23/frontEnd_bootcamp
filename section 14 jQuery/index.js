4fcd2494b4a94f19d3282682537fac2c52add80d
HARI C <harisekar.2306@gmail.com>
2023-08-24 01:27:33 +0530

    section-14: missed commit.

--- section 14 jQuery/index.js
+++ section 14 jQuery/index.js
@@ -1,3 +1,21 @@
 $('button').css('color', 'red');
 
 $('h1').addClass('big-title');
+
+
+// for loop in jQuery
+// default i = 0, for ebery iteration it'll increment itself.
+
+// var i = 1;
+$('button').each(function(e){ 
+    $(this).addClass(`btn-${e}`);
+    // i += 1;
+});
+
+// for(i = 0; i < 5; i++)
+// {
+    document.querySelectorAll('button')[i].addEventListener('click', function(i)
+    {
+        document.querySelector('button')[i].style.color = 'purple';
+    });
+// }
